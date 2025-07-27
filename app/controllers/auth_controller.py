df00eeacb87b23d801cb1cab30612836315fecde
Sam <samikshashanthakumar@gmail.com>
2025-07-27 15:42:46 -0500

    SingUp form added

--- app/controllers/auth_controller.py
+++ app/controllers/auth_controller.py
@@ -1,8 +1,16 @@
 from flask import jsonify, session
 from ..models.user import find_user_by_username, create_user
+from pymongo import MongoClient
+import bcrypt 
+import os
 
 # Author : av42956 
 # Controller : to manage login and register user
+
+client = MongoClient(os.environ.get("MONGO_URI"))
+db = client.hardware_app
+users_collection = db.users
+
 
 def handle_register(request):
     data = request.get_json()
@@ -10,13 +18,20 @@ def handle_register(request):
     password = data.get('password')
 
     if not username or not password:
-        return jsonify({"message": "Username and password required"}), 400
+        return jsonify({'error': 'Username and password required'}), 400
 
-    if find_user_by_username(username):
-        return jsonify({"message": "Username already exists"}), 409
 
-    create_user(username, password)
-    return jsonify({"message": "User registered successfully"}), 201
+    if users_collection.find_one({'username': username}):
+        return jsonify({'error': 'Username already exists'}), 400
+
+    hashed_password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
+    users_collection.insert_one({
+        'username': username,
+        'password': hashed_password.decode('utf-8')  
+    })
+
+    return jsonify({'message': 'User registered successfully'}), 201
 
 def handle_login(request):
     data = request.get_json()
