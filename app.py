d97b77d51021d1037be0fce3d0104ccfccf51a0a
Abhirami Sasikala Rishikesavan <abhisr.mail@gmail.com>
2025-08-04 11:24:09 -0500

    Add Heroku deployment files and config

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
+import os
 from app import create_app
+
 
 app = create_app()
 
 if __name__ == '__main__':
     #app.run(debug=True)
-    app.run(port=8000, debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
 
